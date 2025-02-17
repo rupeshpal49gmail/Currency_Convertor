0531f0831456b5b50b867ccd33b45f6d0ad81306
Rupesh Pal <rupeshpal49@gmail.com>
2025-02-18 01:37:15 +0530

    add currency page

--- src/components/Currency.jsx
+++ src/components/Currency.jsx
@@ -1,7 +1,50 @@
-function CurrencyConvertor() {
+function CurrencyConvertor({
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOptions = [],
+  selectCurrency = "usd",
+  amountDisable = false,
+  currencyDisable = false,
+  className = "",
+}) {
   return (
     <>
-      <h1>Hello Currency Convertor</h1>
+      <div className={`bg-white p-3 rounded-lg text-sm flex `}>
+        <div className="w-1/2">
+          <label className="text-black/40 mb-2 inline-block">{label}</label>
+          <input
+            type="number"
+            className="outline-none w-full bg-transparent py-1 5"
+            placeholder="Amount"
+            disabled={amountDisable}
+            value={amount}
+            onChange={(e) =>
+              onAmountChange && onAmountChange(Number(e.target.value))
+            }
+          />
+        </div>
+        <div className="w-1/2 flex flex-wrap justify-end text-right">
+          <p className="text-black/40 mb-2 w-full">Currency Type</p>
+          <select
+            name=""
+            id=""
+            className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+            value={selectCurrency}
+            onChange={(e) =>
+              onCurrencyChange && onCurrencyChange(e.target.value)
+            }
+            disabled={currencyDisable}
+          >
+            {currencyOptions.map((currency) => (
+              <option key={currency} value={currency}>
+                {currency}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
     </>
   );
 }
